fa43f6a352f5304907e7576d2aa3120577c9d813
Jacob Bell <jbell6@samford.edu>
2023-07-31 14:09:38 -0500

    f

--- components/hero.tsx
+++ components/hero.tsx
@@ -44,7 +44,7 @@ export default function Hero() {
               className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
               data-aos="zoom-y-out"
             >
-              ERC20 Token{" "}
+              ERC20 Token
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 wonderful
               </span>
